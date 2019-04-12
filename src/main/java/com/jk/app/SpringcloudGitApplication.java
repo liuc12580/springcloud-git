40f128aab821e1fa97fcaf21e790a51ec6870156
liuc12580 <958616386@qq.com>
2019-04-12 16:45:52 +0800

    修改启动类文件

--- src/main/java/com/jk/app/SpringcloudGitApplication.java
+++ src/main/java/com/jk/app/SpringcloudGitApplication.java
@@ -10,4 +10,7 @@ public class SpringcloudGitApplication {
         SpringApplication.run(SpringcloudGitApplication.class, args);
     }
 
+    public void aa(){
+        System.out.println("hello world！！！！！！");
+    }
 }
